f15ed3da09c2875dff5317c3959e5913dc68cc59
leo <cedillo47@gmail.com>
2022-04-11 21:39:53 -0400

    created customer order hitstory table

--- migrations/20220412013150_customer_Order_History.js
+++ migrations/20220412013150_customer_Order_History.js
@@ -3,7 +3,17 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('customer_Order_History',(table) => {
+        table.increments('id', {primaryKey:true});
+
+        table.integer('order_id').notNullable();
+        table.foreign('order_id').references('order_Table.id');
+
+        table.integer('customer_id').notNullable();
+        table.foreign('customer_id').references('customer_Table.id'); 
+        
+        table.decimal('order_Total', 8, 2);
+    })  
 };
 
 /**
@@ -11,5 +21,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('customer_Order_History');   
 };
