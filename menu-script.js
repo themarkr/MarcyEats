c48fd5828185e013cf47dbb6baba3eb9cfd637fb
themarkr <markrodriguezaa@gmail.com>
2022-04-18 17:17:33 -0400

    check cart for existing item

--- menu-script.js
+++ menu-script.js
@@ -1,30 +1,63 @@
 // dom content loaded
+const getItemsInCart = async(orderID) => {
+    const response = await fetch(`http://localhost:3000/cart/cart/${orderID}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const cartJson = await response.json();
+    return cartJson.cartInfo
+}
+
+const getMenuItemInfo = async(menuItemID) => {
+    const response = await fetch(`http://localhost:3000/menu/menuItems/${menuItemID}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const menuItemInfoJson = await response.json();
+    return menuItemInfoJson.data[0];
+}
+
+const itemInCart = (cart, itemID) => {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].id == itemID) {
+            return true
+        }
+    }
+    return false;
+}
+
 window.addEventListener('DOMContentLoaded', () => {
-    const orderID = 1;
+    const orderID = 2;
     const buttons = document.querySelectorAll('.add-to-cart-button')
     buttons.forEach(button => {
-        button.addEventListener('click', (event) => {
+        button.addEventListener('click', async(event) => {
             const menuItemID = event.target.id
-            fetch(`http://localhost:3000/cart/cart/${orderID}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    menuId: menuItemID
+            const cart = await getItemsInCart(orderID);
+            if (itemInCart(cart, menuItemID)) {
+                await fetch(`http://localhost:3000/cart/cart/${orderID}/add`, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        menuId: menuItemID
+                    })
                 })
-            })
+            } else {
+                await fetch(`http://localhost:3000/cart/cart/${orderID}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        menuId: menuItemID
+                    })
+                })
+            }
         })
     })
 })
-
-
-// function that queries the database using user id 1 to find the most recent order id that is not complete 
-// other case is all orders are complete, then we would have to create a new order for that customer (focus on guest = userId 1)
-// add to cart button click
-// with that order id every time an add to cart button is clicked, use that id for the route to add it to the order-items table 
-
-
-// cart button click
-// when the cart button is clicked, again use that order id to query into the database with that specific orderId to get all items the order
-// display all items in that order, in the cart
