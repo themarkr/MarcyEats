bcc328d53de45bad8db274925119d0f5821db15a
leo <cedillo47@gmail.com>
2022-04-18 20:19:59 -0400

    finished adding feature

--- renderCartItems.js
+++ renderCartItems.js
@@ -1,51 +1,111 @@
+
+// const helperFunc = require('./addAndSubtractButton');
+
+// console.log(helperFunc)
+
 window.addEventListener('DOMContentLoaded', async (event) => {
     console.log('DOM fully loaded and parsed, hello?');
- 
-    //this is fetching the order Id that is most recent 
 
-    const fetchOrderId = async ()=>{
+    const fetchOrderId = async () => {
         const response = await fetch(` http://localhost:3000/order/mostRecent`)
         const data = await response.json()
         return data.mostRecentOrder[0].id
-    } 
-
-
-    //this is grabing the order information the display all order items in the cart  
-    const data = async () => {
-        const orderId = await fetchOrderId()
-        // console.log("he;lo?")
-        console.log(orderId)
-        const response = await fetch(` http://localhost:3000/cart/cart/${orderId}`)
-        const data = await response.json()
-        return data.cartInfo 
     }
 
-    // console.log(orderId)
+    const data = async () => {
+        const orderId = await fetchOrderId()
+        const response = await fetch(` http://localhost:3000/cart/cart/${orderId}`)
+        const data = await response.json()
+        return data.cartInfo
+    }
+
+    const getTotal = async() => {
+        const tableItems = await data()
+        let total = 0 
+        tableItems.forEach(orderitem => {
+            total += orderitem.price * orderitem.quantity
+        })
+        total = total.toFixed(2);
+        return total
+    }
 
     const orderSubTotal = document.getElementById('orderTotal')
     const table = document.getElementById('foodItems')
-    // const cartButton = document.getElementById('cartB')
+    // let total = 0
+    table.innerHTML = ""
+    const tableItems = await data()
+    
+    // tableItems.forEach(orderitem => {
+    //     total += orderitem.price * orderitem.quantity
+    // })
 
-            let total = 0
-            table.innerHTML = ""
-            const tableItems = await data()
+    tableItems.forEach((orderitem, index) => {
+        let num = orderitem.quantity;
 
-            // console.log("hejhjehbcr")
-            console.log(tableItems)
+        const tablerow = document.createElement('tr');
+        tablerow.setAttribute("id", `tr-${index}`);
 
-            tableItems.forEach(orderitem => {
-                total += orderitem.price * orderitem.quantity
-            })
-
-            tableItems.forEach(orderitem => {
-                const tablerow = document.createElement('tr');
-                tablerow.innerHTML = `<td>${orderitem.name}</td>
-                <td>${orderitem.quantity}</td>
+        tablerow.innerHTML = `<td>${orderitem.name}</td>
+                <td ><span id="minus${index}"> - </span> <span id="quantity${index}">${num} </span> <span id="add${index}"> + </span></td>
                 <td>${orderitem.price}</td>`
 
-                table.appendChild(tablerow)
-            })
+        table.appendChild(tablerow)
+        const minus = document.getElementById(`minus${index}`)
+        const plus = document.getElementById(`add${index}`)
+        let quantity = document.getElementById(`quantity${index}`)
+        // let num = document.getElementById(`quantity${index}`).innerText
+        // console.log(num)
+        // console.log(orderitem.id)
 
-            orderSubTotal.innerHTML = total.toFixed(2);
+        plus.addEventListener("click", async () => {
+            num = num + 1;
+            quantity.innerHTML = num;
+            const orderId = await fetchOrderId()
+            await fetch(`http://localhost:3000/cart/cart/${orderId}/add`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    menuId: orderitem.id
+                })
+            })
+            const total = await getTotal();
+
+            orderSubTotal.innerHTML = total;
+            console.log("hello?")
+        })
+
+        minus.addEventListener("click", async () => {
+            num = num - 1;
+            if (num !== 0) {
+                quantity.innerHTML = num;
+                const orderId = await fetchOrderId()
+                console.log(orderId)
+                await fetch(`http://localhost:3000/cart/cart/${orderId}/subtract`, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        menuId: orderitem.id
+                    })
+                })
+                const total = await getTotal();
+
+                orderSubTotal.innerHTML = total;
+
+                // console.log("hello?")
+            } else {
+                
+            }
+        })
+
+
+    })
+
+    const total = await getTotal();
+
+    orderSubTotal.innerHTML = total;
 
 });
--- routes/cart.js
+++ routes/cart.js
@@ -64,31 +64,14 @@ router.patch('/cart/:id/add', async (req, res) => {
 
 //this route will be used to subtrack from the qunity of an exsisting order item 
 
-//put 
-
-
-//need to make this a put to not impact the other data 
-
-
-
-
-
-router.put('/cart/:id/subtract', async (req, res) => {
+router.patch('/cart/:id/subtract', async (req, res) => {
     const menuItemId = req.body.menuId;
     const orderId = req.params.id;
-    /// dif
-    /// add a way to verify that an order exsist later
-    ///
-
     try {
+        const sql = `UPDATE order_items
+        set quantity = quantity - 1
+        where menu_id = $1 and order_id = $2 returning *;`
 
-        const oldOrder = await pool.query(`SELECT * 
-        from orders 
-        WHERE customer_id = 1
-        order by created_at DESC
-        LIMIT 1;`);
-
-        // finish this later. 
         const response = await pool.query(sql,[menuItemId, orderId])
         res.status(201).json({message: response.rows})
     } catch(err){
